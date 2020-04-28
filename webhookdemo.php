4fa5c049eb5e0ea5fc5951621c5ed880ce5fbb40
shantam sultania <48251619+shantamsultania@users.noreply.github.com>
2020-04-28 13:47:02 +0530

    Add files via upload

---
+++ webhookdemo.php
@@ -0,0 +1,37 @@
+<?php
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if($method == "POST")
+{
+    $requestbody = file_get_contents('php://input');
+    $json = json_decode($requestbody);
+    
+    $text = $json->queryResult->parameters->any;
+    
+    switch ($text) {
+    case 'my name is kamal vaid':
+            $speech = 'hi kamal how can i help you'
+        break;
+    case 'bye':
+            $speech = 'bye see you again';
+            break;
+    default:
+         $speech = 'sorry i was not able to get that please try again';
+            break:
+            
+
+}
+    
+    $response = new \stdClass();
+    $response->speech = "";
+    $response->displayText = "";
+    $response->source = "webhook";
+    echo json_encode($response);
+    
+}
+else
+{
+    echp "only POST method allowed";
+}
+?>
